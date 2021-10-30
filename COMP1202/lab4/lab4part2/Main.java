0bc0324bf839574dc419daf2b7435f70939cfc94
dbraghis <danbraghis@yahoo.com>
2021-10-30 02:54:14 +0100

    Add files via upload

---
+++ COMP1202/lab4/lab4part2/Main.java
@@ -0,0 +1,10 @@
+package lab4part2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		UserGroup userGroup = new UserGroup();
+		userGroup.addSampleData();
+		userGroup.printUsernames();
+	}
+}
