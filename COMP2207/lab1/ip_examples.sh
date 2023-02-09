c9ba01b42f5971f98fa492f7963ac0ae38862f64
D-D.Braghis <ddb1u20@soton.ac.uk>
2023-02-09 16:49:25 +0000

    COMP 2207 lab1

---
+++ COMP2207/lab1/ip_examples.sh
@@ -0,0 +1,11 @@
+#The ip command has taken over from ipconfig and is powerful - and hence complex! Note this is for Linux only.
+#Try these:
+
+#ip a:          short for ip addr - displays all net devices
+#ip -br a:      brief summary - very handy as an alias if used a lot
+#ip -s link:    gives you stats
+#ip link show:  shows details on the link-layer like mac address and mtu
+#ip route:      shows the route table - where each subnet is routed
+#ip neigh:      shows the neighbours (found through ARP for example)
+
+#Be careful with ip commands that add addresses, routes etc - however rebooting clears manual config
---
+++ COMP2207/lab1/traceroute.hs
@@ -0,0 +1,8 @@
+#!/bin/bash
+traceroute google.co.uk
+echo ""
+traceroute google.com
+echo ""
+traceroute cloudflare.com
+echo ""
+traceroute 139.134.5.51
